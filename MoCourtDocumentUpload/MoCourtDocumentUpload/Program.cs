d725c22256cfc099025795ffeeda35d2b6f84829
Justin Schubring <jbschubring@gmail.com>
2018-12-13 18:13:31 -0600

    Initial check in of existing code

---
+++ MoCourtDocumentUpload/MoCourtDocumentUpload/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoCourtDocumentUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // build the ecf exchange document
+            MoEcfExchangeType mo = null;
+            //var x = new MoEcfExchangeType().
+            
+
+            // validate the document before we send it
+
+            // send document
+                // serialize document to text
+                // build request
+                // send request and get response
+
+            // process response
+
+        }
+    }
+}
---
+++ MoCourtDocumentUpload/MoCourtDocumentUpload/Repos/MoEcfServiceRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoCourtDocumentUpload.MoExchangeServiceReference;
+using MoExchangeRequestPayloadType = MoCourtDocumentUpload.MoExchangeServiceReference.MoExchangeRequestPayloadType;
+
+namespace MoCourtDocumentUpload.Repos
+{
+    public class MoEcfServiceRepo
+    {
+        public void Something()
+        {
+            var document = "we need MoEcfExchangeDocument";
+            string theData = "the data";
+
+            var x = new MoExchangeServiceReference.FilingServicePortTypeClient();
+            x.fileNewCase(new MoExchangeServiceReference.MoExchangeRequestPayloadType()
+            {
+                MoExchangeHeader = new MoExchangeServiceReference.MoExchangeHeaderType(),
+                MoExchangeNotification = new MoExchangeServiceReference.MoExchangeNotificationType(),
+                MoExchangeStructuredDataPayload = new MoExchangeServiceReference.MoExchangeStructuredDataPayloadType
+                {
+                    MoExchangeStructuredData = theData
+                }
+            });
+        }
+    }
+}

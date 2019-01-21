44fe405aa2a7c1cddc9730ab2efd334bd4a0194b
Ryan and Sara's <Ryan and Sara's@RYANANDSARAS-PC>
2019-01-20 22:19:34 -0600

    move file functionality

--- MoCourtDocumentUpload/MoCourtDocumentUpload/ProcessFile.cs
+++ MoCourtDocumentUpload/MoCourtDocumentUpload/ProcessFile.cs
@@ -6,6 +6,7 @@ using MoCourtDocumentUpload.Repos;
 using MoCourtDocumentUpload.Services;
 using System;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 
 namespace MoCourtDocumentUpload
@@ -19,29 +20,49 @@ namespace MoCourtDocumentUpload
 			_service = new MoEcfServiceRepo();
 		}
 		public void processFile(string fileName)
-		{		
-			var excel = new ExcelQueryFactory(fileName);
-			var indianaCompanies = from c in excel.Worksheet<ExcelObject>()								
-								   select c;
+		{
+			try
+			{
+				Process(fileName);
+
+				HandleSuccess();
+			}
+			catch (Exception E)
+			{
+				HandleError(E);
+			}
+		
+		}
+
+		private static void Process(string fileName)
+		{
+			GetFile(fileName);
 
 			var doc = BuildTheDocument();
-           
-            var isValid = ValidateTheDocument(doc);
 
-            if (isValid)
-            {
-                var response = SendTheDocument(doc);
-                ProcessResponse(response);
-            }
-            else
-            {
-                HandleInvalidDocument();
-            }
+			var isValid = ValidateTheDocument(doc);
 
-            SendSuccessMessage();
-        }
+			if (isValid)
+			{
+				var response = SendTheDocument(doc);
+				ProcessResponse(response);
+			}
+			else
+			{
+				HandleInvalidDocument();
+			}
+		}
 
-        private static void ProcessResponse(MoExchangeServiceReference.MoExchangeResponsePayloadType response)
+	
+
+		private static void GetFile(string fileName)
+		{
+			var excel = new ExcelQueryFactory(fileName);
+			var indianaCompanies = from c in excel.Worksheet<ExcelObject>()
+								   select c;
+		}
+
+		private static void ProcessResponse(MoExchangeServiceReference.MoExchangeResponsePayloadType response)
         {
             throw new NotImplementedException();
         }
@@ -66,8 +87,13 @@ namespace MoCourtDocumentUpload
             return new BuildDocument().ReturnDocumentXML(new RootObject());
         }
 
-        private static void SendSuccessMessage()
+        private static void HandleSuccess(string fileName)
         {
+		
+			Logger.Info("Message Sent");
+		}
+		private void HandleError(Exception e)
+		{
 			Logger.Info("Message Sent");
 		}
 	}
@@ -77,5 +103,19 @@ namespace MoCourtDocumentUpload
 		public string Test1 { get; set; }
 		[ExcelColumn("Test2")]
 		public string Test2 { get; set; }
+	}
+	public class FileProcess
+	{
+		public string FileName { get; set; }
+		public string FilePath { get; set; }
+		public FileProcess(string filePath)
+		{
+			FileName=  Path.GetFileName(filePath);
+			FilePath = filePath;
+		}
+		public void MoveFile(string path)
+		{
+			File.Move(FilePath, Path.Combine(path, FileName));
+		}
 	}
 }

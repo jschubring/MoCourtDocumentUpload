38c2b62a2cacf0f59488d1b4b47fff40099deb5e
schlueterryan <schlueter_ryan@yahoo.com>
2019-01-29 08:45:58 -0600

    File watch (#2)
    * File Watch start
    
    * Added filewatcher
    
    * move file functionality
    
    * Added file processing steps

---
+++ MoCourtDocumentUpload/MoCourtDocumentUpload/ExistingFileProcessor.cs
@@ -0,0 +1,46 @@
+﻿using MoCourtDocumentUpload.Helpers;
+using System;
+using System.Configuration;
+using System.IO;
+
+namespace MoCourtDocumentUpload
+{
+	public class ExistingFileProcessor
+	{
+		private ProcessFile processFile;	
+		private bool _stopProcessing = false;
+		private string _fileDirectory;
+		public ExistingFileProcessor()
+		{
+			_fileDirectory = ConfigurationManager.AppSettings["filePDirectory"];
+			processFile = new ProcessFile(new DocumentProcessor());
+		}
+		public void StartProcessing()
+		{
+			var filePaths = Directory.EnumerateFiles(_fileDirectory + "Unprocessed", "*.xls", SearchOption.TopDirectoryOnly);
+			foreach (var path in filePaths)
+			{
+				if (_stopProcessing == true) break;
+
+				ProcessFile(path);
+			}
+		}
+
+		private void ProcessFile(string path)
+		{
+			try
+			{
+				processFile.processFile(new CaseFile(path, _fileDirectory));
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(ex);
+			}
+		}
+
+		public void StopProcessing()
+		{
+			_stopProcessing = true;
+		}
+	}
+}
---
+++ MoCourtDocumentUpload/MoCourtDocumentUpload/ProcessFile.cs
@@ -0,0 +1,36 @@
+﻿using MoCourtDocumentUpload.Helpers;
+using System;
+
+namespace MoCourtDocumentUpload
+{
+	public class ProcessFile
+	{
+		private DocumentProcessor _documentWorkflow;
+		public ProcessFile(DocumentProcessor documentWorkflow)
+		{
+			_documentWorkflow = documentWorkflow;
+		}
+		public void processFile(CaseFile newFile)
+		{
+			try
+			{
+				_documentWorkflow.Process(newFile.FullPath);
+				HandleSuccess(newFile);
+			}
+			catch (Exception E)
+			{
+				HandleError(newFile, E);
+			}		
+		}
+        private static void HandleSuccess(CaseFile newFile)
+        {
+			newFile.MoveToProcessedFolder();
+			Logger.Info("Message Sent");
+		}
+		private void HandleError(CaseFile newFile, Exception e)
+		{
+			newFile.MoveToErrorFolder();
+			Logger.Error(e);
+		}
+	}
+}
